597fdab6ee68157971b51f97f4de88ca8fde396e
Jason the noob <jason-ollivierre@hotmail.com>
2024-09-06 15:54:33 +0100

    trabajo general terminado, solo falta añadir props

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="bg-dark text-white text-center py-3 mt-4">
+            <div className="container">
+                <div className="text-center">
+                    © 2024 Copyright
+                </div>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,40 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import NavBar from "../component/navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<NavBar />
+			<main className="container mt-6">
+				<div className="row">
+					<div className="col-12">
+						<Jumbotron />
+					</div>
+					<div className="col-12 mt-4">
+						<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+							{/* Display cards in a responsive grid */}
+							<div className="col">
+								<Card />
+							</div>
+							<div className="col">
+								<Card />
+							</div>
+							<div className="col">
+								<Card />
+							</div>
+							<div className="col">
+								<Card />
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+			<Footer />
+		</>
 	);
 };
 
